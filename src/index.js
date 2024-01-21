431960d7d392baaa3a7cf1aa76c19814a3024998
Ivan <77786271+IvanBuzduha@users.noreply.github.com>
2024-01-21 19:43:42 +0200

    Add main files

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {BrowserRouter as Router } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+  <AuthProvider>
+       <Router> 
+        <App />
+       </Router> 
+   </AuthProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
