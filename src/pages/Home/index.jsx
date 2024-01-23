3ed93c8652f66be41269777dd14f7c75d3b867f1
Ivan <77786271+IvanBuzduha@users.noreply.github.com>
2024-01-23 20:05:34 +0200

    add text

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,6 +19,7 @@ function Home() {
           <Typography variant="body1" gutterBottom>
             This is demo app with login, registration and updating profile
             flows.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
           </Typography>
         </Grid>
       </Grid>
