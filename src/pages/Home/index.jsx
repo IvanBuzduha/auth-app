3d4630f9d33245f803c96c3d480def21dc2933f0
Ivan <77786271+IvanBuzduha@users.noreply.github.com>
2024-01-27 17:10:54 +0200

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,24 @@
 import { Grid, makeStyles, Container, Typography } from "@material-ui/core";
 
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(4),
-  },
+    padding: theme.spacing(4)
+  }
 }));
 
 function Home() {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="sm"  className={classes.root}>
+    <Container maxWidth="sm" className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Typography variant="h2" gutterBottom>
             Homepage
           </Typography>
           <Typography variant="body1" gutterBottom>
-            This is demo app with login, registration and updating profile
-            flows.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            This is demo app with login, registration and updating profile flows. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </Typography>
         </Grid>
       </Grid>
